383f4d224c79be963579f65797f43d3276e0a3dd
M B <m.belka@email.cz>
2026-06-22 16:25:24 +0200

    initial structure and shared integration event contracts

---
+++ src/OrderSystem.Contracts/IntegrationEvents/IIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderSystem.Contracts.IntegrationEvents;
+
+public interface IIntegrationEvent
+{
+    Guid EventId { get; }
+
+    string EventType { get; }
+
+    DateTime OccurredAtUtc { get; }
+
+    Guid CorrelationId { get; }
+}
